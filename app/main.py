7d4b0b9ebf70531f21ff7265b905c745e153f0c6
boscolio <nickrules@gmail.com>
2020-11-19 18:35:42 -0600

    adding matrix png to app

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.api import predict
 description = """
 This app is used to predict whether a Kickstarter will be successful based on the funding goal, campaign length, description and category.'
 
-<img src="images/kickstarter correlation matrix.png" width="40%" />
+<img src="https://github.com/boscolio/kickstarter_success/blob/main/images/kickstarter%20correlation%20matrix.png?raw=true" width="40%" />
 """
 
 app = FastAPI(
